7188569a20deb4b1695c67cf4b1ed7805d7bc267
root <root@server.linux.com>
2018-03-08 17:49:11 +0530

    Test file modification

---
+++ index.php
@@ -0,0 +1,3 @@
+echo "   <a href="http://192.168.1.30/public">Student 1 environment</a>";
+echo "   <a href="http://192.168.1.30/public">Student 2 environment</a>";
+echo "   <a href="http://192.168.1.30/public">Student 3 environment</a>";
